d453944f45e460a684762c96a09ddecf4de3cb83
JassPotter <jasspotter919@gmal.com>
2020-08-16 23:21:26 +0530

    Start Induction form

---
+++ AmzonConstruction/Controller/InductionForm/InductionFormVC.swift
@@ -0,0 +1,30 @@
+//
+//  InductionFormVC.swift
+//  AmzonConstruction
+//
+//  Created by Jashu Prajapati on 8/16/20.
+//  Copyright © 2020 JDC. All rights reserved.
+//
+
+import UIKit
+
+class InductionFormVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
