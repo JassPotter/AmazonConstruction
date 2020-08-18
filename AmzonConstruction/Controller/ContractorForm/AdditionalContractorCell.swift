36dddde1bffbb1bcb37546108737529dec9d0548
JassPotter <jasspotter919@gmal.com>
2020-08-18 11:22:14 +0530

    add

---
+++ AmzonConstruction/Controller/ContractorForm/AdditionalContractorCell.swift
@@ -0,0 +1,29 @@
+//
+//  AdditionalContractorCell.swift
+//  AmzonConstruction
+//
+//  Created by Dhaval Nagar on 14/08/20.
+//  Copyright © 2020 JDC. All rights reserved.
+//
+
+import UIKit
+
+class AdditionalContractorCell: UITableViewCell {
+
+    
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var btnDelete: UIButton!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
